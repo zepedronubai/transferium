d23828e8c2fc5d801bae8c2cdc4cf5cf0f6add5c
zepedronubai <zepedro.noworks@gmail.com>
2025-06-27 23:14:47 +0100

    yoyoy

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import LanguageSwitcher from '../utils/languageSwitcher';
 import { FaCarAlt } from 'react-icons/fa';
 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Hero from './components/hero';
 import AboutUs from './components/aboutUs';
 import Testemonials from './components/testemonials';
