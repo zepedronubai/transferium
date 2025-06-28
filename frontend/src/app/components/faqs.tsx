ff484b5a9b910dff59799aa7719a3a77dcc80cdf
zepedronubai <zepedro.noworks@gmail.com>
2025-06-28 13:11:16 +0100

    yoloi

--- frontend/src/app/components/faqs.tsx
+++ frontend/src/app/components/faqs.tsx
@@ -4,7 +4,7 @@ import FaqsComponent from './faqs/faqsComponent';
 export default function Faqs() {
   return (
     <div className='flex md:flex-row flex-col w-full max-w-[1100px] items-center  md:gap-10 gap-0  md:px-4 px-2'>
-      <div className='flex flex-col items-start md:gap-4 gap-3 w-full'>
+      <div className='flex flex-col md:items-start items-center md:gap-4 gap-3 w-full'>
         <div className='flex flex-col  md:items-start items-center '>
           <div className='md:text-base text-xs font-bold text-customYellow'>
             FAQ&apos;S
--- frontend/src/app/components/testemonials.tsx
+++ frontend/src/app/components/testemonials.tsx
@@ -13,7 +13,7 @@ export default function Testemonials() {
         />
         <div className='absolute top-0 left-0 w-full h-full -z-10 overflow-hidden bg-black opacity-40'></div>
       </div>
-      <div className='flex flex-col w-full max-w-[1100px] md:items-center items-start md:py-20 py-10 gap-10 md:px-4 px-2'>
+      <div className='flex flex-col w-full max-w-[1100px] md:items-center items-center md:py-20 py-10 gap-10 md:px-4 px-2'>
         <div className='flex flex-col  md:items-center items-center '>
           <div className='md:text-base text-xs font-bold text-customYellow'>
             TESTEMONIALS
